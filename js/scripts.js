8c0b77055ea3af493f83be39e1bdd6f28dce4a3b
PauloBSS <pbss2007@gmail.com>
2021-08-17 23:16:39 -0300

    atualizacao modal-alert & success

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,36 @@ function limparFormns() {
     document.getElementById('formnsModalCadastro').reset();
 }
 
+// Método para limpar alert de erros
+function limparAlert(){
+    document.getElementById('alertName').innerHTML = "";
+    document.getElementById('alertEmail').innerHTML = ""; 
+}
+ 
+// Método para manipular os botões do modal cadastro
+function responseSucesso(){
+    $('#btnLoading').prop("style",'display:none');
+	$('#btnCadastrar').show();
+    $('#alertSucesso').show();
+    $('#alertInf').show();
+}
+
+// Método para resetar o modal de cadastro
+function fecharModal(){
+    $('#alertSucesso').prop("style",'display:none');
+    $('#alertInf').prop("style",'display:none');
+    $('#cadastroModal').on('hidden.bs.modal', function (){
+        $(this).find('form').trigger('reset');
+    })
+    limparAlert();
+}
+
 // Método para 'Cadastrar' usuário no banco de dados
 async function salvarUsuario() {
-
     $('#btnCadastrar').prop("style",'display:none');
 	$('#btnLoading').show();
+    limparAlert();
+
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
 
@@ -21,17 +46,44 @@ async function salvarUsuario() {
         data: JSON.stringify(user),
         contentType: "application/json; charset=utf-8",
         success: function (response) {
-            alert("Cadastro realizado com sucesso!\n"
-            +"Você receberá um email de nossa Equipe");
+            //alert("Cadastro realizado com sucesso!\n"
+            //+"Você receberá um email de nossa Equipe");
             limparFormns();
-            $('#btnLoading').prop("style",'display:none');
-			$('#btnCadastrar').show();
+            responseSucesso();
+            limparAlert();
         }
 
     }).fail(function (xhr, status, errorThrown) {
-          alert(xhr.responseJSON.message);
-          limparFormns();
-          $('#btnLoading').prop("style",'display:none');
-		  $('#btnCadastrar').show();
+        if(xhr.responseJSON.error == "Erro de validação"){
+            alertError(xhr.responseJSON.errors);
+        }else if (xhr.responseJSON.error == "Duplicate email") {
+        //$('#alertEmail').append(" " + xhr.responseJSON.message)
+            document.getElementById('alertEmail').innerHTML = xhr.responseJSON.message
+        } else {
+            alert(xhr.responseJSON.message);
+        }
+        //   alert(xhr.responseJSON.message);
+        //   alertError(xhr.responseJSON.errors);
+        limparFormns();
+        $('#btnLoading').prop("style",'display:none');
+		$('#btnCadastrar').show();
     });
+}
+
+let errorsClass = [
+    erro = {
+       fieldName : 'String',
+       message : 'String'
+    }
+];
+
+function alertError(obj){
+    // Objeto de referencia
+    errorsClass = obj;
+    // aplicando o lambda no array de objeto e utilizando operado ternário
+    errorsClass.forEach(
+       item => (item.fieldName == "name" ? 
+        document.getElementById('alertName').innerHTML = item.message :
+        document.getElementById('alertEmail').innerHTML = item.message)
+    );
 }
